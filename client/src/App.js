2e486f20cdb3972585d7eebdb21ed4b7c8c68cc2
Albert <zawadaa10@gmail.com>
2023-03-14 12:17:51 +0100

    Responsive navbar done

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// import routes
+import Homepage from './routes/Homepage';
+import LoginPage from './routes/LoginPage';
+import RegisterPage from './routes/RegisterPage';
+import PageNotFound from './routes/PageNotFound';
+
+// router
+const router = createBrowserRouter([
+  { path: '/', element: <Homepage /> },
+  { path: '/signin', element: <LoginPage /> },
+  { path: '/signup', element: <RegisterPage /> },
+  { path: '/*', element: <PageNotFound /> },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
