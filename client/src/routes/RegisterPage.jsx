e88298c3adda2b24248c8d7a3b566704cc72fb4c
Albert <zawadaa10@gmail.com>
2023-03-21 11:22:38 +0100

    Some troubles with header 'Access-Control-Allow-Origin'

--- client/src/routes/RegisterPage.jsx
+++ client/src/routes/RegisterPage.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from '../api/axios';
 import Navbar from '../components/Navbar';
 import Register from '../components/Register';
 
 const RegisterPage = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get('/auth/islogin')
+      .then((response) => {
+        console.log(response.data);
+        if (response.data === true) {
+          navigate('/platform');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <>
       <Navbar />
