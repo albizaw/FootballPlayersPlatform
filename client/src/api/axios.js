2c2a8675aeea377bc04d1c0c00dd1cbd69f1fc8e
Albert <zawadaa10@gmail.com>
2023-03-27 17:49:20 +0200

    Changes - client

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import API from './apidata.js';
 
 export default axios.create({
   withCredentials: true,
-  baseURL: 'http://localhost:8000',
+  baseURL: process.env.REACT_APP_BASE_URL,
 });
 
 // export const api = axios.create({
 //   baseURL: API.BASE_URL,
 // });
+//baseURL: 'http://localhost:8000',
